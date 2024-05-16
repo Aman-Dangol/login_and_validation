8dfe302547c7f5e8e06e4811157b35b510a6e69b
Aman Dangol <amandangol423@gmail.com>
2024-05-16 13:59:59 +0545

    working with cookies

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 // setting up database
 const mysql = require("mysql");
 const connection = mysql.createConnection({
@@ -12,27 +11,34 @@ const connection = mysql.createConnection({
 const app = express();
 
 let error = "";
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // set up view engine
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
+  console.log(req.headers.cookie);
+  res.cookie("cast", "");
   res.render("login", { err: error });
   error = "";
 });
 
+app.get("/sign-up", (req, res) => {
+  res.render("signup", { err: error });
+});
 app.post("/sign-up", (req, res) => {
   const { email, password } = req.body;
   connection.query(
     `insert into users(email,password) values ('${email}','${password}')`,
     (err, data) => {
-      if (err.code == "ER_DUP_ENTRY") {
+      if (err) {
         error = "email already in use ";
         res.redirect("/");
         return;
       }
-      res.send("success");
+      res.redirect("/");
     }
   );
 });
