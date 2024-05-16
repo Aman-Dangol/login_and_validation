3ba79a9d331d14bdc8f856a20941c0ef0ef6dbcb
Aman Dangol <amandangol423@gmail.com>
2024-05-16 10:39:25 +0545

    setting up sign-up

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const express = require("express");
 
+// setting up database
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "aman",
+  password: "root",
+  database: "login",
+});
+
 const app = express();
 
+let error = "";
+app.use(express.urlencoded({ extended: false }));
+
+// set up view engine
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("<h1>Home-Page</h1>");
+  res.render("login", { err: error });
+  error = "";
+});
+
+app.post("/sign-up", (req, res) => {
+  const { email, password } = req.body;
+  connection.query(
+    `insert into users(email,password) values ('${email}','${password}')`,
+    (err, data) => {
+      if (err.code == "ER_DUP_ENTRY") {
+        error = "email already in use ";
+        res.redirect("/");
+        return;
+      }
+      res.send("success");
+    }
+  );
 });
 
 app.listen(8000);
